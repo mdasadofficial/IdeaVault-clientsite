7868dbea2dcf9a58255cb741726a32bae89527b9
mdasadofficial <mdasadofficial1@gmail.com>
2026-06-28 00:58:27 +0600

    chore: Update dependencies and improve database connection settings

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import dns from "node:dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
